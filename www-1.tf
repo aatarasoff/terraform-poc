b550159432367052119962fe983711272c694772
Aleksandr Tarasov <aatarasoff@gmail.com>
2019-09-23 14:43:41 +0300

    Update www-1.tf

--- www-1.tf
+++ www-1.tf
@@ -2,6 +2,6 @@ resource "digitalocean_droplet" "www-1" {
     image = "ubuntu-18-04-x64"
     name = "www-1"
     region = "nyc1"
-    size = "1Gb"
+    size = "2Gb"
     private_networking = true
     }
